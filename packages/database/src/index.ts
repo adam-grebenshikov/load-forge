119c277bd5c232914f4ca6dc99502c9a90abaa9f
Adam <adam.grebenshikov@gmail.com>
2026-01-30 14:00:00 +0400

    feat(database): add prisma client with pg adapter

---
+++ packages/database/src/index.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { config } from "@loadforge/config";
+
+const adapter = new PrismaPg({ connectionString: config.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
