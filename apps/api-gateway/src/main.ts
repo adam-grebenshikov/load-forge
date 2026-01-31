966e5dc5efb8f011ab63858563195f7b9520a9d8
Adam <adam.grebenshikov@gmail.com>
2026-01-31 14:34:12 +0400

    chore(api-gateway): initialize API gateway with NestJS

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+
+import { NestFactory } from "@nestjs/core";
+
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.setGlobalPrefix("api/v1");
+
+  const port = Number(process.env.PORT) || 3000;
+
+  await app.listen(port, "0.0.0.0");
+}
+
+bootstrap();
